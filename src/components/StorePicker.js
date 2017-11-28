120b31f12cec3c56687ddcf16e62602f48cceaf7
Adam Champagne <adamchampagne@Adams-iMac.fios-router.home>
2017-11-28 14:04:19 -0500

    added store picker component

---
+++ src/components/StorePicker.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Create store picker component
+class StorePicker extends React.Component {
+ render(){
+     return <p>Hello</p>
+ }
+}
+
+export default StorePicker;
